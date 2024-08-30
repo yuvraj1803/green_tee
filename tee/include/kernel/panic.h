4328ce43c54ee4029a578ff994705abbc0b2dbeb
yuvraj1803 <ysakshith@gmail.com>
2024-08-30 12:52:59 +0530

    moved halt() to panic.h

--- tee/include/kernel/panic.h
+++ tee/include/kernel/panic.h
@@ -1,6 +1,7 @@
 #ifndef __PANIC_H__
 #define __PANIC_H__
 
+void halt();
 void panic(char* msg);
 
 #endif
--- tee/kernel/panic.c
+++ tee/kernel/panic.c
@@ -1,7 +1,6 @@
 #include <kernel/panic.h>
 #include <lib/stdio.h>
 
-void halt();
 
 void panic(char* msg){
 	printf("[PANIC]: %s\n", msg);
