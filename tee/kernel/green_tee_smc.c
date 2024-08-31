bb798e8109399e05d005656194b635666700628b
yuvraj1803 <ysakshith@gmail.com>
2024-08-31 12:06:44 +0530

    TEE side entry done SMC now passes address of exception vector table to SPD

--- tee/kernel/green_tee_smc.c
+++ tee/kernel/green_tee_smc.c
@@ -1,4 +1,7 @@
 #include <smc/green_tee_smc.h>
+#include <lib/memory.h>
+
+extern uint64_t vector_table;
 
 void green_tee_smc_entry_done(){
     struct arm_smccc_call_params params;
@@ -7,5 +10,8 @@ void green_tee_smc_entry_done(){
 	params.fast_call = 1;
 	params.smc64 = 1;
 	params.service_call = GREEN_TEE_SMC_ENTRY_DONE;
+    memset(params.args, 0, sizeof(params.args));
+	params.args[1] = (uint64_t) &vector_table;
+
 	arm_smccc_call(&params, &ret);
 }
