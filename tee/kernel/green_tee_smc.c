d8deb6480b0eeae75f19df81415a15451814ae65
yuvraj1803 <ysakshith@gmail.com>
2024-09-01 11:48:10 +0530

    added panic() if green_tee_entry_done() fails

--- tee/kernel/green_tee_smc.c
+++ tee/kernel/green_tee_smc.c
@@ -1,5 +1,6 @@
 #include <smc/green_tee_smc.h>
 #include <lib/memory.h>
+#include <kernel/panic.h>
 
 extern uint64_t vector_table;
 
@@ -14,4 +15,9 @@ void green_tee_smc_entry_done(){
 	params.args[1] = (uint64_t) &vector_table;
 
 	arm_smccc_call(&params, &ret);
+
+	if(ret.call_failed){
+		panic("Green TEE Entry Done SMC Failed.\n");
+	}
+
 }
